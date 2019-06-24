785ee865745ac6ac6393b9ff943d17d3c996b5cd
lenovo <lenovo@DESKTOP-NN19MM0>
2019-06-24 22:11:25 +0530

    second commit

---
+++ Fundamentals/src/D4.java
@@ -0,0 +1,23 @@
+
+
+public class D4 {
+	public static void main(String[] args)
+	{
+		
+		int i=10;
+		if(i==0)
+		{
+			System.out.println("the number is zero");
+		}
+		if(i>=0)
+		{
+			System.out.println("the number is positive");
+		}
+		if(i<=0)
+		{
+			System.out.println("the number is negative");
+		}
+		
+	}
+
+}
