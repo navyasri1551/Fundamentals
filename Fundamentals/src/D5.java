29f492f231d6316b3a44e0c7681a28cdcc8e44e0
lenovo <lenovo@DESKTOP-NN19MM0>
2019-06-24 22:15:36 +0530

    second commit

---
+++ Fundamentals/src/D5.java
@@ -0,0 +1,12 @@
+
+public class D5 {
+	public static void main(String[] args)
+	{
+		int i=5;
+		if(i%2==0)
+			System.out.println("the number is even");
+		else
+			System.out.println("the number is odd");
+	}
+
+}
