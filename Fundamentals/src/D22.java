f34f0bbf11efd576f54467d92557f8d25085aa8f
lenovo <lenovo@DESKTOP-NN19MM0>
2019-06-26 10:30:13 +0530

    Third commit

--- Fundamentals/src/D22.java
+++ Fundamentals/src/D22.java
@@ -1,12 +1,25 @@
 
 public class D22 {
 	public static void main(String args[]) {
+		int count=0;
+		int i = 1;
+		while(true)
+		{
+			
+			if(i%2==0 && i%3==0 && i%5==0)
+			{
+				System.out.println(i);
+				count++;
 				
-		System.out.println("\n\nDivided by 2 & 3 & 5: ");			
-		for (int i=1; i<180; i++) {
-			if (i%2==0 && i%3==0 && i%5==0) System.out.print(i +", ");			
+			}
+			if(count==5)
+				break;
+			i++;
+			
+			
 		}
-		System.out.println("\n");
+		
+		
   }
 
 }
--- Fundamentals/src/D8.java
+++ Fundamentals/src/D8.java
@@ -2,10 +2,10 @@
 public class D8 {
 	public static void main(String[] args)
 	{
-		char c=5;
+		char c= 'a';
 		if((c>='A'&&c<='Z')||(c>='a'&&c<='z'))
 			System.out.println("Alphabet");
-		else if(c>=0&&c<=9)
+		else if(c>='0'&&c<='9')
 				System.out.println("Digit");
 		else
 			System.out.println("Special characters");
